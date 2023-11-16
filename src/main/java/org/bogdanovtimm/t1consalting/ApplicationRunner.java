35b26b9f3e752df444a525382eea52b89f05f828
Tim Bogdanov <101057307+BogdanovTimm@users.noreply.github.com>
2023-11-17 00:10:47 +0300

    dockerfile and docker-compose added

--- src/main/java/org/bogdanovtimm/t1consalting/ApplicationRunner.java
+++ src/main/java/org/bogdanovtimm/t1consalting/ApplicationRunner.java
@@ -3,11 +3,6 @@ package org.bogdanovtimm.t1consalting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// [v] Add swagger
-// [ ] Save swagger as PDF
-// [v] Integration test
-// [v] Unit test for service
-// [v] Different REST methods (for case sensitive and case insensetive)
 // [ ] Docker compose
 
 @SpringBootApplication
